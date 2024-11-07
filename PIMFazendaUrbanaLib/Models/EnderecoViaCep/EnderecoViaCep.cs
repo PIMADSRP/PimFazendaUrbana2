87474e1c7fb904624122b016538d20108b4be6bd
Gustavo Lupaquini <gustavo_spinellilupaquini@outlook.com>
2024-11-07 17:39:21 -0300

    main

--- PIMFazendaUrbanaLib/Models/EnderecoViaCep/EnderecoViaCep.cs
+++ PIMFazendaUrbanaLib/Models/EnderecoViaCep/EnderecoViaCep.cs
@@ -8,15 +8,15 @@ namespace PIMFazendaUrbanaLib
 {
     public class EnderecoViaCep
     {
-        public string Cep { get; set; }
-        public string Logradouro { get; set; }
-        public string Complemento { get; set; }
-        public string Unidade { get; set; }
-        public string Bairro { get; set; }
-        public string Localidade { get; set; }
-        public string Uf { get; set; }
-        public string Estado { get; set; }
-        public string Regiao { get; set; }
+        public string cep { get; set; }
+        public string logradouro { get; set; }
+        public string complemento { get; set; }
+        public string unidade { get; set; }
+        public string bairro { get; set; }
+        public string localidade { get; set; }
+        public string uf { get; set; }
+        public string estado { get; set; }
+        public string regiao { get; set; }
         public string Ibge { get; set; }
         public string Gia { get; set; }
         public string Ddd { get; set; }
